130a168e5ef43c9beb8974c93bc1dda8a6e57f43
lucnovais <lucas.novais@indicium.tech>
2022-10-14 18:55:47 -0300

    Adicionando chamada para a funcao que transforma um AFD em um arquivo DOT

--- main.c
+++ main.c
@@ -14,16 +14,21 @@ int main(int argc, char *argv[])
     char *caminho_arquivo = NULL;
     char *caminho_saida = NULL;
 
+    /* Este laco verifica quais flags foram usadas para rodar o programa e chama as funcoes adequadas */
     for(int i = 0; i < argc; i++)
     {
         if(strcmp(argv[i], DOT) == 0)
+        {
             caminho_arquivo = argv[i + 1];
-
+        }
         if(strcmp(argv[i], OUTPUT) == 0)
+        {
             caminho_saida = argv[i + 1];
+        }
     }
 
     AFD afd = arquivo_para_afd(caminho_arquivo);
+    afd_para_dot(afd, caminho_saida);
 
     return 0;
 }
