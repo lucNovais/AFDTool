719a6a7dd29428045f9070d678b6e329ab232c16
lucnovais <lucas.novais@indicium.tech>
2022-10-14 18:54:18 -0300

    Adicionando documentacao e tamanhos dos conjuntos do AFD

--- afd.h
+++ afd.h
@@ -4,6 +4,21 @@
 #define MAX_LINHAS 200
 #define MAX_CARACTERES 100
 
+/*
+ * Estrutura de dados utilizada para representar os AFDs
+ * -----------------------------------------------------
+ * 
+ * `estados` (char[][]): vetor que representa quais os estados do AFD
+ * `alfabeto` (char[][]): vetor que representa quais os símbolos do alfabeto do AFD
+ * `transicoes` (char[][]): vetor que representa quais as transicoes no formato '{estado} {simbolo} {estado}'
+ * `estado_inicial` (char[]): string que representa qual o estado inicial do AFD
+ * `estados_finais` (char[][]): vetor que representa quais os estados finais do AFD 
+ * `tamanhos` (int[]): vetor de inteiros que contem os tamanhos de cada um dos vetores acima
+ *      0 -> tamanho estados
+ *      1 -> tamanho alfabeto
+ *      2 -> tamanho transicoes
+ *      3 -> tamanho finais
+ */
 typedef struct afd
 {
     char estados[MAX_LINHAS][MAX_CARACTERES];
@@ -11,6 +26,7 @@ typedef struct afd
     char transicoes[MAX_LINHAS][MAX_CARACTERES];
     char estado_inicial[MAX_CARACTERES];
     char estados_finais[MAX_LINHAS][MAX_CARACTERES];
+    int tamanhos[4];
 } AFD;
 
 #endif
